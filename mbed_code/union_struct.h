a8e60f304e9aee88018fd1e895ead92dc7cc4399
ChanghyeonKim93 <hyun91015@gmail.com>
2023-03-27 23:37:21 +0900

    update: timer.h/.cpp and union struct

--- mbed_code/union_struct.h
+++ mbed_code/union_struct.h
@@ -4,19 +4,34 @@
 #include "mbed.h"
 
 // Unions
+typedef union SHORT_UNION_{
+    int16_t short_;
+    unsigned char  bytes_[2];
+} SHORT_UNION;
+
 typedef union USHORT_UNION_{
     uint16_t ushort_;
     unsigned char  bytes_[2];
 } USHORT_UNION;
 
+typedef union INT_UNION_{
+    int32_t int_;
+    unsigned char bytes_[4];
+} INT_UNION;
+
 typedef union UINT_UNION_{
     uint32_t uint_;
-    unsigned char  bytes_[4];
+    unsigned char bytes_[4];
 } UINT_UNION;
 
 typedef union FLOAT_UNION_{
     float float_;
     unsigned char bytes_[4];   
 } FLOAT_UNION;
+
+typedef union DOUBLE_UNION_{
+    double double_;
+    unsigned char bytes_[8];
+} DOUBLE_UNION;
 
 #endif
--- ros_code/include/union_struct.h
+++ ros_code/include/union_struct.h
@@ -2,15 +2,21 @@
 #define _UNION_STRUCT_H_
 #include <iostream>
 
+// Unions
 typedef union SHORT_UNION_{
     int16_t short_;
-    unsigned char bytes_[2];
+    unsigned char  bytes_[2];
 } SHORT_UNION;
 
 typedef union USHORT_UNION_{
     uint16_t ushort_;
-    unsigned char bytes_[2];
+    unsigned char  bytes_[2];
 } USHORT_UNION;
+
+typedef union INT_UNION_{
+    int32_t int_;
+    unsigned char bytes_[4];
+} INT_UNION;
 
 typedef union UINT_UNION_{
     uint32_t uint_;
@@ -19,7 +25,12 @@ typedef union UINT_UNION_{
 
 typedef union FLOAT_UNION_{
     float float_;
-    unsigned char bytes_[4];
+    unsigned char bytes_[4];   
 } FLOAT_UNION;
+
+typedef union DOUBLE_UNION_{
+    double double_;
+    unsigned char bytes_[8];
+} DOUBLE_UNION;
 
 #endif
